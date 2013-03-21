7e53db60b4c3580491676b1c9b85e9e28cb8e945
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-03-21 14:18:01 -0400

    Load and render GPS trace on map.

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,8 @@ var pathHead;
 var map;
 var ourPathHead;
 
-function drawSparkline(elementId, width, height, dataX, domainX, dataY, domainY) {
+function drawSparkline(elementId, width, height, dataX, domainX, dataY,
+        domainY) {
     // create an SVG element inside the element that fills 100% of the div
     var graph = d3.select(elementId).append("svg:svg").attr("width",
         "100%").attr("height", "100%");
@@ -29,20 +30,50 @@ function drawSparkline(elementId, width, height, dataX, domainX, dataY, domainY)
     graph.append("svg:path").attr("d", line(dataX));
 }
 
+var position = {latitude: undefined, longitude: undefined};
+
+function handleMessage(message) {
+    if(message.name === "latitude") {
+        position.latitude = message.value;
+    } else if(message.name === "longitude") {
+        position.longitude = message.value;
+    }
+
+    if(position.latitude && position.longitude) {
+        pathHead.setLatLng([position.latitude, position.longitude]);
+        path.addLatLng(pathHead.getLatLng());
+        map.fitBounds(path.getBounds());
+        position.latitude = position.longitude = undefined;
+    }
+}
+
+function loadTrace() {
+    $.ajax({
+      url: $("#trace-1").attr("href"),
+      success: function(data) {
+          $.each(data.split("\n"), function(i, line) {
+              if(line) {
+                  handleMessage(JSON.parse(line));
+              }
+          });
+      },
+      dataType: "text"
+    });
+}
+
 $(document).ready(function() {
     var startingPosition = new L.LatLng(39.970806, -119.387054);
     map = L.map('map').setView(startingPosition, 13);
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-        maxZoom: 14
+        maxZoom: 16
     }).addTo(map);
 
-    ourPath = L.polyline([], {color: 'blue'}).addTo(map);
-    ourPathHead = L.marker(startingPosition).addTo(map);
     // create a red polyline from an arrays of LatLng points
-    path = L.polyline([], {color: 'red'}).addTo(map);
-    pathHead = L.circle(startingPosition, 100).addTo(map);
+    path = L.polyline([], {color: 'red', width: 20}).addTo(map);
+    pathHead = L.circle(startingPosition, 25).addTo(map);
 
+    loadTrace();
     drawSparkline("#speed", 200, 50, _.range(200), [0, 10],
             _.range(200), [0, 10]);
 
