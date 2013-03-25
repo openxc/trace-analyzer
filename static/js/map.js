f4e2b24405461e73c7ca7b8d3d892659df7a2f0c
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-03-24 21:16:25 -0400

    Collapse nearby stops onto one another.

--- static/js/map.js
+++ static/js/map.js
@@ -5,6 +5,7 @@ var hoverMapMarker;
 // TODO hard coded for NYC demo, but could look this up dynamically in the
 // future
 var SPEED_LIMIT_KPH = 48.28;
+var MEAN_RADIUS_EARTH_KM = 6373;
 
 var mapHoverHandler = {
     on: function(timestamp, trace) {
@@ -34,12 +35,6 @@ var recordsWithPosition =  function(records) {
     });
 }
 
-var positionRecords = function(records) {
-    return _.map(recordsWithPosition(records), function(record) {
-        return [record.latitude, record.longitude];
-    });
-}
-
 // TODO we could optimize this and only go over the path once, yielding
 // positions to these functions each time and then calling a finish() method,
 // but the trace is small now (~600) for a few minutes) so i'll not prematurely
@@ -47,25 +42,33 @@ var positionRecords = function(records) {
 var markLongestStops = function(trace) {
     if(!_.has(trace.mapLayerGroups, "longestStops")) {
         var longestStops = L.featureGroup();
-        var lastPosition = undefined;
         var stops = [];
         var stoppedAt = undefined;
+        var records = recordsWithPosition(trace.records);
+        var lastPosition = _.first(records);
 
-        _.each(recordsWithPosition(trace.records), function(record) {
-            var position = [record.latitude, record.longitude];
-            if(lastPosition) {
-                if(record.vehicle_speed < 1 && position[0] === lastPosition[0]
-                        && position[1] === lastPosition[1]) {
-                    if(!stoppedAt) {
+        _.each(records, function(record) {
+            if(record.vehicle_speed < 1 &&
+                    recordedPositionEqual(record, lastPosition)){
+                if(!stoppedAt) {
+                    // TODO could just use odometer...
+                    if(stops.length > 0 &&
+                            distanceKm(_.last(stops).start, record) < .2) {
+                        stoppedAt = stops.pop().start;
+                    } else {
                         stoppedAt = record;
                     }
-                } else if(stoppedAt) {
-                    stops.push({start: stoppedAt, end: record});
-                    stoppedAt = undefined;
                 }
+            } else if(stoppedAt) {
+                stops.push({start: stoppedAt, end: record});
+                stoppedAt = undefined;
             }
-            lastPosition = position;
+            lastPosition = record;
         });
+
+        if(stoppedAt) {
+            stops.push({start: stoppedAt, end: lastPosition});
+        }
 
         var nonTrivialStops = _.filter(stops, function(stop) {
             return stop.end.timestamp - stop.start.timestamp > 5;
@@ -83,20 +86,42 @@ var markLongestStops = function(trace) {
     map.addLayer(trace.mapLayerGroups.longestStops);
 }
 
+var recordedPositionEqual = function(a, b) {
+    return a.latitude && a.longitude && b.latitude && b.longitude &&
+        a.latitude === b.latitude && a.longitude && b.longitude;
+}
+
+function deg2rad(deg) {
+    return deg * Math.PI / 180;
+}
+
+var distanceKm = function(first, second) {
+    var deltaLat = deg2rad(second.latitude) - deg2rad(first.latitude);
+    var deltaLon = deg2rad(second.longitude) - deg2rad(first.longitude);
+    var a = Math.pow(Math.sin(deltaLat / 2), 2) +
+            Math.cos(deg2rad(first.latitude)) *
+            Math.cos(deg2rad(second.latitude)) *
+            Math.pow(Math.sin(deltaLon / 2), 2);
+    var c = 2 * Math.atan2(Math.sqrt(a),Math.sqrt(1 - a));
+    var distance =  c * MEAN_RADIUS_EARTH_KM;
+    console.log("distance is " + distance);
+    return distance;
+}
+
 var highlightSlowSections = function(trace) {
     if(!_.has(trace.mapLayerGroups, "slowSections")) {
         var slowSections = L.multiPolyline([]);
-        var lastPosition = undefined;
+        var lastPosition = _.first(trace.records);
         var options = {color: 'red', width: 25};
         var path = L.polyline([], options);
+
         _.each(trace.records, function(record) {
             var position = [record.latitude, record.longitude];
-            if(position[0] && position[1] &&
-                    (!lastPosition || lastPosition != position) &&
+            if(!recordedPositionEqual(lastPosition, record) &&
                     record.vehicle_speed > 1 &&
                     record.vehicle_speed < SPEED_LIMIT_KPH) {
                 path.addLatLng(position);
-                lastPosition = position;
+                lastPosition = record;
             } else if(path.getLatLngs().length > 0) {
                 slowSections.addLayer(path);
                 path = L.polyline([], options);
@@ -127,11 +152,12 @@ var renderGpsTrace = function(trace) {
 
     if(!_.has(trace.mapLayerGroups, "base")) {
         var path = L.polyline([], {color: 'green', width: 20});
-        var lastPosition = undefined;
-        _.each(positionRecords(trace.records), function(position) {
-            if((!lastPosition || lastPosition != position)) {
-                path.addLatLng(position);
-                lastPosition = position;
+        var records = recordsWithPosition(trace.records);
+        var lastPosition = _.first(records);
+        _.each(records, function(record) {
+            if(!recordedPositionEqual(lastPosition, record)) {
+                path.addLatLng([record.latitude, record.longitude]);
+                lastPosition = record;
             }
         });
 
